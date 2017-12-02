3273fd61997ada8607c79f1cacb37db29dcf704f
brandon.assing <brandon.assing@ibm.com>
2017-12-02 17:36:50 -0500

    added stock info

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
+	function refreshList(){
+		$.get("/api/symbols", function(data){
+          	var symbol;
+            for(var i in data){
+            	symbol = JSON.stringify(data[i].symbol).replace(/['"]+/g, '').toUpperCase();
+            	$("#liked-list").append('<li id="' + symbol + '-liked" class="DocumentItem"><h3>' + symbol + '</h3></li>');
+            	var url = "https://api.iextrading.com/1.0/stock/" + symbol + "/quote";
+            	$.get(url, function(stockData){
+            		var name = JSON.stringify(stockData.companyName).replace(/['"]+/g, '');
+            		var price = JSON.stringify(stockData.latestPrice).replace(/['"]+/g, '');
+            		var volume = JSON.stringify(stockData.latestVolume).replace(/['"]+/g, '');
+            		var symbol = JSON.stringify(stockData.symbol).replace(/['"]+/g, '');
+					$('#'+ symbol + '-liked').append('<p>' + name + '</p><h4>Latest Price: ' + price + '</h4><h4>Latest Volume: ' + volume + '</h4>');
+	            });
+            }
+        });
+	}
+
+	refreshList();
 	$("#placeholder-box").draggable({
 		revert: false,
 	});
 
 	$("#refresh").click(function(){
-
-          $.get("/api/symbols", function(data){
-            //window.alert(JSON.stringify(data));
-            for(var i in data){
-            	var symbol = JSON.stringify(data[i].symbol).replace(/['"]+/g, '').toUpperCase();
-            	$("#liked-list").append('<li class="DocumentItem"><h3>' + symbol + '</h3></li>');
-            }
-            //$.each(data, function (i, item) {
-            	
-            	//$("#liked-list ul").append('<li class="DocumentItem"><h3>'+ item.symbol +'</h3></li>');
-            //}
-        });
-
+          refreshList();
 	});
-})
+});
