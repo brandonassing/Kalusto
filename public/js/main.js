9af1c2ce34c6b94b6f80faa0ffb00a29aa6a2383
Minho Lee <minho.lee.93@hotmail.com>
2017-12-02 14:27:16 -0500

    properly swipes right/left and returns

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,34 @@
+var $box = $("#placeholder-box");
+var $box_left = $box.offset().left;
+var timer1;
 $(document).ready(function() {
-	$("#placeholder-box").draggable({
-		revert: false,
+	$box.draggable({
+		revert: true,
+		containment: [-300,0,300,100],
 	});
 
-	
-})
+	timer1 = setInterval(function() {
+		checkForSwipe()
+	}, 50);
+});
+
+function checkForSwipe() {
+	if ($box.offset().left > $box_left + $box.width()) {
+		console.log('Swiped Right!');
+		clearInterval(timer1);
+		wait();
+	} else if ($box.offset().left < $box_left - $box.width()) {
+		console.log('Swiped Left!');
+		clearInterval(timer1);
+		wait();
+	}
+}
+
+function wait() {
+	// Recheck for swipe after waiting for 2 seconds
+	window.setTimeout(function() {
+		timer1 = setInterval(function() {
+			checkForSwipe()
+		}, 50);
+	}, 2000);
+}
