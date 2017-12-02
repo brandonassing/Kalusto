ac34e9ce9aa9f6abef9531278830b1edea72a0c4
bdcorps <bdcorps@gmail.com>
2017-12-02 18:56:49 -0500

    add swipe functions/transaction

--- public/js/main.js
+++ public/js/main.js
@@ -1,141 +1,188 @@
 var $box = $("#placeholder-box");
-var $box_left = $box.offset().left, $box_top = $box.offset().top;
+var $box_left = $box.offset().left,
+    $box_top = $box.offset().top;
 var timer1;
-var labelX = [], labelY = [];
-const company = 'AAPL';
+var labelX = [],
+    labelY = [];
+var company = 'AAPL';
 
 $(document).ready(function() {
-	function refreshList(){
-		$.get("/api/symbols", function(data){
-          	var symbol;
-            for(var i in data){
-            	symbol = JSON.stringify(data[i].symbol).replace(/['"]+/g, '').toUpperCase();
-            	$("#liked-list").append('<li id="' + symbol + '-liked" class="DocumentItem"><h3>' + symbol + '</h3></li>');
-            	var url = "https://api.iextrading.com/1.0/stock/" + symbol + "/quote";
-            	$.get(url, function(stockData){
-            		var name = JSON.stringify(stockData.companyName).replace(/['"]+/g, '');
-            		var price = JSON.stringify(stockData.latestPrice).replace(/['"]+/g, '');
-            		var volume = JSON.stringify(stockData.latestVolume).replace(/['"]+/g, '');
-            		var symbol = JSON.stringify(stockData.symbol).replace(/['"]+/g, '');
-					$('#'+ symbol + '-liked').append('<p>' + name + '</p><h4>Latest Price: ' + price + '</h4><h4>Latest Volume: ' + volume + '</h4>');
-	            });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.keyCode == 37) {
+            //left arrow
+            SwipeLeft();
+
+        } else if (event.keyCode == 38) {
+            //up arrow
+            SwipeUp();
+        } else if (event.keyCode == 39) {
+            //right arrow
+            SwipeRight();
+        }
+    });
+
+    function refreshList() {
+        $.get("/api/symbols", function(data) {
+            var symbol;
+            for (var i in data) {
+                symbol = JSON.stringify(data[i].symbol).replace(/['"]+/g, '').toUpperCase();
+                $("#liked-list").append('<li id="' + symbol + '-liked" class="DocumentItem"><h3>' + symbol + '</h3></li>');
+                var url = "https://api.iextrading.com/1.0/stock/" + symbol + "/quote";
+                $.get(url, function(stockData) {
+                    var name = JSON.stringify(stockData.companyName).replace(/['"]+/g, '');
+                    var price = JSON.stringify(stockData.latestPrice).replace(/['"]+/g, '');
+                    var volume = JSON.stringify(stockData.latestVolume).replace(/['"]+/g, '');
+                    var symbol = JSON.stringify(stockData.symbol).replace(/['"]+/g, '');
+                    $('#' + symbol + '-liked').append('<p>' + name + '</p><h4>Latest Price: ' + price + '</h4><h4>Latest Volume: ' + volume + '</h4>');
+                });
             }
         });
-	}
+    }
 
-	refreshList();
-	$("#placeholder-box").draggable({
-		revert: false,
-	});
+    refreshList();
+    $("#placeholder-box").draggable({
+        revert: false,
+    });
 
-	$("#refresh").click(function(){
-          refreshList();
-	});
-	Chart.defaults.global.responsive = true;
-	Chart.defaults.global.legend.display = false;
+    $("#refresh").click(function() {
+        refreshList();
+    });
+    Chart.defaults.global.responsive = true;
+    Chart.defaults.global.legend.display = false;
 
-	$box.draggable({
-		revert: true,
-		containment: [-$(window).width()/2 + $box.width()/2 + 20, -50, $(window).width()/2 - $box.width()/2 ,100],
-	});
+    $box.draggable({
+        revert: true,
+        containment: [-$(window).width() / 2 + $box.width() / 2 + 20, -50, $(window).width() / 2 - $box.width() / 2, 100],
+    });
 
-	timer1 = setInterval(function() {
-		checkForSwipe()
-	}, 50);
+    timer1 = setInterval(function() {
+        checkForSwipe()
+    }, 50);
 
-	$.ajax({
-		url: `https://api.iextrading.com/1.0/stock/${company}/chart`,
-		success: function(res) {
-			console.log(res);
-			for (var i = 0; i < res.length; i++) {
-				var date = res[i].date, closePrice = res[i].close;
-				labelX.push(date);
-				labelY.push(closePrice);
-			}
-			$.get({
-				url: `https://api.iextrading.com/1.0/stock/${company}/quote`,
-				success: function(res) {
-					$("#companyName").html(res.companyName+ ' ('+ res.symbol+ ')');
-					$("#latestPrice").html('$' +res.latestPrice);
-					$("#priceChange").html('('+res.change.toString().slice(0,1) + '$' + res.change.toString().slice(1));
 
-					var changePercent = (Math.abs(res.changePercent.toFixed(2)) == 0) ? 
-															res.changePercent :  
-															res.changePercent.toFixed(2);
+    refreshView();
 
-					$("#priceChange").append(' / ' + changePercent + '%)');
-					if (res.change > 0) {
-						$("#priceChange").addClass('tealColor');
-					} else {
-						$("#priceChange").addClass('redColor');
-					}
-					$("#today").html('Today');
-				}
-			});
 
-			var ctx = document.getElementById("stockChart").getContext('2d');
-			var myChart = new Chart(ctx, {
-				type: 'line',
-				data: {
-					labels: labelX,
-					datasets: [{
-						data: labelY,
-						fill: false,
-						borderColor: '#9320a2',
-						pointBackgroundColor: 'white',
-					}]
-				},
-				options: {
-					scales: {
-						xAxes:[{
-							scaleLabel: {
-								display: true,
-								labelString: 'Date',
-							},
-							ticks: {
-								fontSize: 8,
-							}
-						}],
-						yAxes: [{
-							ticks: {
-								callback: function(value, index, values) {
-									return '$'+value;
-								},
-								fontSize: 8,
-							},
-							scaleLabel: {
-								display: true,
-								labelString: 'Price',
-							}
-						}]
-					}
-				}
-			});
-		},
-	})
 });
 
 function checkForSwipe() {
-	if ($box.offset().left + $box.width() > $(window).width() - 20) {
-		console.log('Swiped Right!');
-		clearInterval(timer1);
-		wait();
-	} else if ($box.offset().left < 10) {
-		console.log('Swiped Left!');
-		clearInterval(timer1);
-		wait();
-	} else if ($box.offset().top - $box_top < -50) {
-		console.log('Swiped Top!');
-		clearInterval(timer1);
-		wait();
-	}
+    if ($box.offset().left + $box.width() > $(window).width() - 20) {
+        console.log('Swiped Right!');
+        SwipeRight();
+        clearInterval(timer1);
+        wait();
+    } else if ($box.offset().left < 10) {
+        console.log('Swiped Left!');
+        SwipeLeft();
+        clearInterval(timer1);
+        wait();
+    } else if ($box.offset().top - $box_top < -50) {
+        console.log('Swiped Top!');
+        SwipeTop();
+        clearInterval(timer1);
+        wait();
+    }
+}
+
+function refreshView() {
+    $.ajax({
+        url: `https://api.iextrading.com/1.0/stock/${company}/chart`,
+        success: function(res) {
+            console.log(res);
+            for (var i = 0; i < res.length; i++) {
+                var date = res[i].date,
+                    closePrice = res[i].close;
+                labelX.push(date);
+                labelY.push(closePrice);
+            }
+            $.get({
+                url: `https://api.iextrading.com/1.0/stock/${company}/quote`,
+                success: function(res) {
+                    $("#companyName").html(res.companyName + ' (' + res.symbol + ')');
+                    $("#latestPrice").html('$' + res.latestPrice);
+                    $("#priceChange").html('(' + res.change.toString().slice(0, 1) + '$' + res.change.toString().slice(1));
+
+                    var changePercent = (Math.abs(res.changePercent.toFixed(2)) == 0) ?
+                        res.changePercent :
+                        res.changePercent.toFixed(2);
+
+                    $("#priceChange").append(' / ' + changePercent + '%)');
+                    if (res.change > 0) {
+                        $("#priceChange").addClass('tealColor');
+                    } else {
+                        $("#priceChange").addClass('redColor');
+                    }
+                    $("#today").html('Today');
+                }
+            });
+
+            var ctx = document.getElementById("stockChart").getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labelX,
+                    datasets: [{
+                        data: labelY,
+                        fill: false,
+                        borderColor: '#9320a2',
+                        pointBackgroundColor: 'white',
+                    }]
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Date',
+                            },
+                            ticks: {
+                                fontSize: 8,
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                callback: function(value, index, values) {
+                                    return '$' + value;
+                                },
+                                fontSize: 8,
+                            },
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Price',
+                            }
+                        }]
+                    }
+                }
+            });
+        },
+    })
+}
+
+function SwipeLeft() {
+    $.get("https://kalusto-py.mybluemix.net/api/symbol", function(res) {
+        console.log(res);
+    });
+
+    company = "TSLA"
+    refreshView();
+    // document.getElementById("formLeft").submit();
+
+}
+
+function SwipeUp() {
+    document.getElementById("transactionForm").submit();
+}
+
+function SwipeRight() {
+    //	 document.getElementById("formRight").submit();
 }
 
 function wait() {
-	// Recheck for swipe after waiting for 2 seconds
-	window.setTimeout(function() {
-		timer1 = setInterval(function() {
-			checkForSwipe()
-		}, 50);
-	}, 2000);
+    // Recheck for swipe after waiting for 2 seconds
+    window.setTimeout(function() {
+        timer1 = setInterval(function() {
+            checkForSwipe()
+        }, 50);
+    }, 2000);
 }
