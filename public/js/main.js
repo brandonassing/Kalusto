9681e9f1e8ca6d86ee88091f5990430756c0461d
brandon.assing <brandon.assing@ibm.com>
2017-12-02 16:06:44 -0500

    added dynamic liked stock list

--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,19 @@ $(document).ready(function() {
 		revert: false,
 	});
 
-	
+	$("#refresh").click(function(){
+
+          $.get("/api/symbols", function(data){
+            //window.alert(JSON.stringify(data));
+            for(var i in data){
+            	var symbol = JSON.stringify(data[i].symbol).replace(/['"]+/g, '').toUpperCase();
+            	$("#liked-list").append('<li class="DocumentItem"><h3>' + symbol + '</h3></li>');
+            }
+            //$.each(data, function (i, item) {
+            	
+            	//$("#liked-list ul").append('<li class="DocumentItem"><h3>'+ item.symbol +'</h3></li>');
+            //}
+        });
+
+	});
 })
